c1003d55280dc704f537e6c99901cb56433d9e0b
Kamila <kamilla.adamson@gmail.com>
2023-02-19 02:25:47 +0600

    added a Experience1

---
+++ Experience1/Program.cs
@@ -0,0 +1,45 @@
+﻿int[,] getArray()
+{
+  int[,] array = new int[3, 3];
+  Random random = new Random();
+
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = random.Next(0, 10);
+      Console.Write(array[i, j] + "\t");
+    }
+    Console.WriteLine();
+  }
+  return array;
+}
+
+int[,] counterclockwise(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    Console.Write(array[2, i] + "\t"); // 2 указывает индекс строки
+  }
+
+  for (int j = array.GetLength(1) - 1; j >= 0; j--)
+  {
+    Console.Write(array[j, 2] + "\t");
+  }
+
+  for (int i = array.GetLength(0) - 2; i >= 0; i--)
+  {
+    Console.Write(array[0, i] + "\t");
+  }
+
+  for (int j = 0; j < array.GetLength(0) - 1; j++)
+  {
+    Console.Write(array[1, j] + "\t");
+  }
+  return array;
+
+}
+
+int[,] array = getArray();
+Console.WriteLine();
+int[,] newArray = counterclockwise(array);
